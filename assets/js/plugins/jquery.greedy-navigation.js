005218bfd966ebfc5c362c740ff2e61f0add9bbd
spchai <spchai1002@outlook.com>
2025-10-06 17:28:15 +0800

    header

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -1,133 +1,86 @@
 /*
-* Greedy Navigation - Mobile Fix
+* Greedy Navigation
+*
+* http://codepen.io/lukejacksonn/pen/PwmwWV
+*
 */
 
 var $nav = $('#site-nav');
 var $btn = $('#site-nav button');
 var $vlinks = $('#site-nav .visible-links');
+var $vlinks_persist_tail = $vlinks.children("*.persist.tail");
 var $hlinks = $('#site-nav .hidden-links');
 
-// 获取所有可移动的导航项（不包括持久化的项）
-var $movableLinks = $vlinks.find('li:not(.persist)');
 var breaks = [];
 
 function updateNav() {
-    // 如果是移动端，使用不同的逻辑
-    if (window.innerWidth <= 1024) {
-        updateNavMobile();
-    } else {
-        updateNavDesktop();
+
+  var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
+
+  // The visible list is overflowing the nav
+  if ($vlinks.width() > availableSpace) {
+
+    while ($vlinks.width() > availableSpace && $vlinks.children("*:not(.persist)").length > 0) {
+      // Record the width of the list
+      breaks.push($vlinks.width());
+
+      // Move item to the hidden list
+      $vlinks.children("*:not(.persist)").last().prependTo($hlinks);
+
+      availableSpace = $btn.hasClass("hidden") ? $nav.width() : $nav.width() - $btn.width() - 30;
+
+      // Show the dropdown btn
+      $btn.removeClass("hidden");
     }
-    updateLayout();
+
+    // The visible list is not overflowing
+  } else {
+
+    // There is space for another item in the nav
+    while (breaks.length > 0 && availableSpace > breaks[breaks.length - 1]) {
+      // Move the item to the visible list
+      if ($vlinks_persist_tail.children().length > 0) {
+        $hlinks.children().first().insertBefore($vlinks_persist_tail);
+      } else {
+        $hlinks.children().first().appendTo($vlinks);
+      }
+      breaks.pop();
+    }
+
+    // Hide the dropdown btn if hidden list is empty
+    if (breaks.length < 1) {
+      $btn.addClass('hidden');
+      $btn.removeClass('close');
+      $hlinks.addClass('hidden');
+    }
+  }
+
+  // Keep counter updated
+  $btn.attr("count", breaks.length);
+
+  // update masthead height and the body/sidebar top padding
+  var mastheadHeight = $('.masthead').height();
+  $('body').css('padding-top', mastheadHeight + 'px');
+  if ($(".author__urls-wrapper button").is(":visible")) {
+    $(".sidebar").css("padding-top", "");
+  } else {
+    $(".sidebar").css("padding-top", mastheadHeight + "px");
+  }
+
 }
 
-function updateNavDesktop() {
-    // 桌面端逻辑 - 重置所有项目到可见列表
-    $hlinks.children().appendTo($vlinks);
-    $hlinks.addClass('hidden');
-    $btn.addClass('hidden');
-    $btn.removeClass('close');
-    
-    var availableSpace = $nav.width() - ($btn.hasClass('hidden') ? 0 : $btn.width() + 30);
-    
-    // 如果可见列表溢出
-    if ($vlinks.width() > availableSpace) {
-        breaks = [];
-        
-        while ($vlinks.width() > availableSpace && $vlinks.children('li:not(.persist)').length > 0) {
-            breaks.push($vlinks.width());
-            // 移动最后一个非持久化项目到隐藏列表
-            $vlinks.children('li:not(.persist)').last().prependTo($hlinks);
-            availableSpace = $nav.width() - $btn.width() - 30;
-        }
-        
-        $btn.removeClass('hidden');
-    } else {
-        // 尝试将项目移回可见列表
-        while (breaks.length > 0 && availableSpace > breaks[breaks.length - 1] && $hlinks.children().length > 0) {
-            $hlinks.children().first().appendTo($vlinks);
-            breaks.pop();
-        }
-        
-        if ($hlinks.children().length === 0) {
-            $btn.addClass('hidden');
-            $hlinks.addClass('hidden');
-        }
-    }
-    
-    $btn.attr('count', $hlinks.children().length);
-}
+// Window listeners
 
-function updateNavMobile() {
-    // 移动端逻辑 - 将所有非持久化项目移到隐藏菜单
-    $vlinks.find('li:not(.persist)').appendTo($hlinks);
-    
-    // 如果有隐藏项目，显示按钮
-    if ($hlinks.children().length > 0) {
-        $btn.removeClass('hidden');
-    } else {
-        $btn.addClass('hidden');
-    }
-    
-    $hlinks.addClass('hidden');
-    $btn.removeClass('close');
-}
-
-function updateLayout() {
-    var mastheadHeight = $('.masthead').outerHeight(true);
-    $('body').css('padding-top', mastheadHeight + 'px');
-    
-    if ($(".author__urls-wrapper button").is(":visible")) {
-        $(".sidebar").css("padding-top", "");
-    } else {
-        $(".sidebar").css("padding-top", mastheadHeight + "px");
-    }
-}
-
-// 事件监听
-$(window).on('resize', function() {
-    setTimeout(updateNav, 100);
+$(window).on('resize', function () {
+  updateNav();
+});
+screen.orientation.addEventListener("change", function () {
+  updateNav();
 });
 
-if (screen.orientation) {
-    screen.orientation.addEventListener('change', function() {
-        setTimeout(updateNav, 150);
-    });
-}
-
-$btn.on('click', function(e) {
-    e.stopPropagation();
-    $hlinks.toggleClass('hidden');
-    $(this).toggleClass('close');
+$btn.on('click', function () {
+  $hlinks.toggleClass('hidden');
+  $(this).toggleClass('close');
 });
 
-// 点击外部关闭下拉菜单
-$(document).on('click', function(e) {
-    if (!$(e.target).closest('#site-nav').length) {
-        $hlinks.addClass('hidden');
-        $btn.removeClass('close');
-    }
-});
-
-// 防止下拉菜单内的点击事件冒泡
-$hlinks.on('click', function(e) {
-    e.stopPropagation();
-});
-
-// 初始化
-$(document).ready(function() {
-    // 立即执行一次
-    updateNav();
-    
-    // 等待页面完全加载
-    $(window).on('load', function() {
-        setTimeout(updateNav, 200);
-    });
-    
-    // 防抖的resize处理
-    var resizeTimer;
-    $(window).on('resize', function() {
-        clearTimeout(resizeTimer);
-        resizeTimer = setTimeout(updateNav, 150);
-    });
-});
+updateNav();
